97c2d1d913cb84f4be04cc20386841bebe8b2604
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-11-09 21:54:48 +0900

    テストを失敗させる

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,17 +1,7 @@
 package com.example.demo
 
-import com.example.demo.service.HelloService
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class DemoApplicationTests {
-
-	@Test
-	fun TaroHello() {
-		val helloService = HelloService();
-		val sayMessage = helloService.say("Taro")
-		Assertions.assertEquals("Taro Hello!", sayMessage)
-	}
 }
--- src/test/kotlin/com/example/demo/service/HelloServiceTest.kt
+++ src/test/kotlin/com/example/demo/service/HelloServiceTest.kt
@@ -1,5 +1,15 @@
 package com.example.demo.service
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 internal class HelloServiceTest
+
+@Test
+fun TaroHello() {
+    val helloService = HelloService();
+    val sayMessage = helloService.say("Taro")
+    // あえてテストに失敗させる
+    Assertions.assertEquals("Taro Hello!!", sayMessage)
+}
